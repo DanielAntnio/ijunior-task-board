2036c59cb2d46a7d2e0429e26a747713e4c2b618
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-09 11:57:30 -0300

    style: corrige "Pedente" para "Pendente"

--- src/components/NewServiceForm.tsx
+++ src/components/NewServiceForm.tsx
@@ -5,7 +5,7 @@ interface Props {
   addOrder: (newOrder: serviceOrder) => void;
 }
 
-const PossibleStatus = ["Pedente", "Processando", "Concluido"];
+const PossibleStatus = ["Pendente", "Processando", "Concluido"];
 
 export default function NewServiceForm({ addOrder }: Props) {
   const [name, setName] = useState<string>("");
