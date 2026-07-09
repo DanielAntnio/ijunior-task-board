e94a003e66ce178ab14088a1b9066945a20fc424
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-09 11:57:51 -0300

    feat: cria ServiceCard

---
+++ src/components/ServiceCard.tsx
@@ -0,0 +1,12 @@
+import type serviceOrder from "../types";
+
+export default function ServiceCard({ name, model, defect, status}: serviceOrder) {
+  return (
+    <li className="flex flex-col bg-blue-200 items-center p-4 border rounded-md max-w-60">
+      <h4 className="font-bold text-wrap text-center text-xl mb-2">{name}</h4>
+      <span className="italic underline mb-2">{model}</span>
+      <p className="text-base text-center">{defect}</p>
+      <span className="mt-4">{status}</span>
+    </li>
+  );
+}
