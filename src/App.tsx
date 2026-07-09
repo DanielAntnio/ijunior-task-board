387ab753dad9b761fec6a7b90a6c28496705294d
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-09 11:58:37 -0300

    feat: estruta a pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,45 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import NewServiceForm from "./components/NewServiceForm";
+import ServiceCard from "./components/ServiceCard";
+import type serviceOrder from "./types";
+
+const PossibleStatus = ["Pendente", "Processando", "Concluido"];
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  const [orders, setOrders] = useState<serviceOrder[]>([]);
+
+  return (
+    <>
+      <Header />
+      <main className="px-4 py-2">
+        <section>
+          <NewServiceForm
+            addOrder={(newOrder: serviceOrder) =>
+              setOrders((prev) => [...prev, newOrder])
+            }
+          />
+        </section>
+
+        {PossibleStatus.map((pStatus, pos) => {
+          const ordersFiltered = orders.filter(
+            (order) => order.status === pStatus,
+          );
+
+          if (ordersFiltered.length < 1) return;
+
+          return (
+            <section key={pos} className="mt-4">
+              <h3 className="text-3xl mb-4">{pStatus}</h3>
+              <ul className="flex flex-row overflow-x-auto scroll-smooth scrollbar-none gap-y-2 gap-x-4">
+                {ordersFiltered.map((order, pos) => (
+                  <ServiceCard {...order} key={pos} />
+                ))}
+              </ul>
+            </section>
+          );
+        })}
+      </main>
+    </>
+  );
 }
