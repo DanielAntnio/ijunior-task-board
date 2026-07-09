ae4e337b2e3bf528ca491c10b6e063580f05da80
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-09 12:21:45 -0300

    feat: adiciona o estilo condicional com base no status

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -2,7 +2,9 @@ import type serviceOrder from "../types";
 
 export default function ServiceCard({ name, model, defect, status}: serviceOrder) {
   return (
-    <li className="flex flex-col bg-blue-200 items-center p-4 border rounded-md max-w-60">
+    <li className={`flex flex-col items-center p-4 border rounded-md max-w-60
+      ${status === "Aberto" ? "bg-green-400" : "bg-gray-400 opacity-80"}
+    `}>
       <h4 className="font-bold text-wrap text-center text-xl mb-2">{name}</h4>
       <span className="italic underline mb-2">{model}</span>
       <p className="text-base text-center">{defect}</p>
