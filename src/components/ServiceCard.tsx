6de1ef2ef2d856106de5bae01b00297eedb14b11
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-14 17:51:50 -0300

    feat: atualiza o Service Card conforme o tipo ServiceOrder

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,15 +1,28 @@
-import type serviceOrder from "../types";
+import { useEffect, useState } from "react";
+import type { ServiceOrder } from "../types";
+import { getClientById } from "../services/clientService";
 
-type Props = serviceOrder;
+type Props = ServiceOrder;
 
-export default function ServiceCard({ name, model, defect, status}: Props) {
+export default function ServiceCard({ client_id, device, issue, status}: Props) {
+  const [ name, setName ] = useState("...")
+
+  useEffect(() => {
+    async function load() {
+      const data = await getClientById(client_id);
+      setName(data.name ?? "Sem nome");
+    }
+
+    load();
+  }, [])
+
   return (
-    <li className={`flex flex-col items-center p-4 border dark:border-black rounded-md max-w-60
-      ${status === "Aberto" ? "bg-green-400" : "bg-gray-400 opacity-80"}
+    <li className={`flex flex-col items-center p-4 border dark:border-black rounded-md
+      ${status === "done" ? "bg-green-400" : "bg-gray-400 opacity-80"}
     `}>
       <h4 className="font-bold text-wrap text-center text-xl mb-2">{name}</h4>
-      <span className="italic underline mb-2">{model}</span>
-      <p className="text-base text-center">{defect}</p>
+      <span className="italic underline mb-2">{device}</span>
+      <p className="text-base text-center text-balance">{issue}</p>
     </li>
   );
 }
