b0152673c4562a023b9965701dc428a7d953869d
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-14 17:51:13 -0300

    feat: adiciona uma navbar ao Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router";
+
 export default function Header() {
   return (
-    <header className="bg-slate-200 dark:bg-slate-900 px-2 py-0.5 max-h-8">
-        <h3 className="text-gray-900 dark:text-gray-100 font-bold">iReapir</h3>
+    <header className="flex felx-row justify-between bg-slate-200 dark:bg-slate-900 px-2 py-0.5 max-h-8">
+        <h1 className="text-gray-900 dark:text-gray-100 font-bold">iReapir</h1>
+        <nav className="flex flex-row text-gray-900 dark:text-gray-100 gap-3">
+          <Link to="/">Dashboard</Link>
+          <Link to="/clients">Clientes</Link>
+        </nav>
     </header>
   )
 }
