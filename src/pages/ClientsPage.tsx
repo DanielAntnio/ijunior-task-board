880f741c16d8f432fe8d504e86470eeeb3d35292
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-17 12:15:32 -0300

    feat: Implementa sistema de carregamento e componente ClientCard em ClientsPage

--- src/pages/ClientsPage.tsx
+++ src/pages/ClientsPage.tsx
@@ -1,12 +1,17 @@
-import { useEffect, useState } from 'react';
-import { getAllClients, deleteClient } from '../services/clientService';
-import type { Client } from '../types';
-import NewClientForm from '../components/NewClientForm';
+import { useEffect, useState } from "react";
+import ClientCard from "../components/ClientCard";
+import NewClientForm from "../components/NewClientForm";
+import { getAllClients, deleteClient } from "../services/clientService";
+import {
+  deleteServiceOrder,
+  getAllServiceOrders,
+} from "../services/serviceOrderService";
+import Loader from "../components/Loader";
+import type { Client, CreateClientData } from "../types";
 
 const ClientsPage = () => {
   const [clients, setClients] = useState<Client[]>([]);
-  const [update, setUpdate] = useState<number>(0)
-
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     async function load() {
@@ -15,26 +20,49 @@ const ClientsPage = () => {
     }
 
     load();
-  }, [update]);
+    setLoading(false);
+  }, []);
 
   async function handleDelete(id: number) {
     await deleteClient(id);
-    setClients(prev => prev.filter(c => c.id !== id));
+    setClients((prev) => prev.filter((c) => c.id !== id));
+
+    const data = await getAllServiceOrders();
+    const filteredData = data.filter((order) => order.client_id === id);
+    filteredData.forEach(async (order) => {
+      await deleteServiceOrder(order.id);
+    });
+  }
+
+  function clientExists({ name, phone, email }: CreateClientData) {
+    const client = clients.find(
+      (client) =>
+        client.name === name &&
+        client.email === email &&
+        client.phone === phone,
+    );
+
+    return client !== undefined;
   }
 
   return (
     <>
-    <NewClientForm reRender={() => setUpdate(prev => prev + 1)}/>
-    <ul>
-      {clients.map(client => (
-        <li key={client.id}>
-          {client.name}
-          <button onClick={() => handleDelete(client.id)}>Excluir</button>
-        </li>
-      ))}
-    </ul>
+      <NewClientForm clientExists={clientExists} setClients={setClients} />
+      {loading ? (
+        <Loader />
+      ) : (
+        <ul className="mt-4 flex flex-col gap-2">
+          {clients.map((client) => (
+            <ClientCard
+              key={client.id}
+              {...client}
+              handleDelete={handleDelete}
+            />
+          ))}
+        </ul>
+      )}
     </>
   );
 };
 
-export default ClientsPage
+export default ClientsPage;
