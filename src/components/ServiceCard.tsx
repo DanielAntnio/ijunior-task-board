f02d7b52ffaaf76a7993b90d8221d4093625910f
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-17 11:57:58 -0300

    refactor: elimina redundancia em ServiceCard (handleDelete vira parametro opicional)

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,11 +1,9 @@
 import type { ServiceOrder } from "../types";
-import { deleteServiceOrder } from "../services/serviceOrderService";
 
 type Props = Omit<
   ServiceOrder & {
-    setOrders: React.Dispatch<React.SetStateAction<ServiceOrder[]>>;
+    handleDelete?: (id: number) => Promise<void>;
     name: string;
-    showButtons?: boolean;
   },
   "created_at" | "client_id"
 >;
@@ -16,14 +14,8 @@ const ServiceCard = ({
   device,
   issue,
   status,
-  showButtons = false,
-  setOrders,
+  handleDelete,
 }: Props) => {
-  async function handleDelete(id: number) {
-    await deleteServiceOrder(id);
-    setOrders((prev) => prev.filter((c) => c.id !== id));
-  }
-
   return (
     <li
       className={`flex flex-col items-center p-4 border dark:border-black rounded-md
@@ -33,7 +25,7 @@ const ServiceCard = ({
       <h4 className="font-bold text-wrap text-center text-xl mb-2">{name}</h4>
       <span className="italic underline mb-2">{device}</span>
       <p className="text-base text-center text-balance">{issue}</p>
-      {showButtons && (
+      {handleDelete !== undefined && (
         <button
           className="hover:cursor-pointer mt-2 bg-slate-50 px-2 rounded-md border"
           onClick={() => handleDelete(id)}
