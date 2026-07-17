8ffeb3c40e10bff0511bbb885cd06790f8dab639
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-17 12:18:46 -0300

    style: altera estilo de main para poder centralizar Loader

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function MainLayout() {
   return (
     <div className="w-full h-dvh flex flex-col">
       <Header />
-      <main className="grow px-4 py-2 bg-slate-100 dark:bg-slate-800">
+      <main className="flex flex-col h-max grow px-4 py-2 bg-slate-100 dark:bg-slate-800 *:not-last:mb-4">
         <Outlet />
       </main>
     </div>
