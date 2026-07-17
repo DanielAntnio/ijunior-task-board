075e18d2257f7857b638c69356f3f48daf31d922
Daniel Antonio <danielantnio8679@gmail.com>
2026-07-17 12:32:35 -0300

    style: makes header elements wrap when out of space

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router";
 
 const Header = () => {
   return (
-    <header className="flex felx-row justify-between bg-slate-200 dark:bg-slate-900 px-2 py-0.5 max-h-8">
+    <header className="inline-flex justify-between bg-slate-200 dark:bg-slate-900 px-2 py-0.5">
       <h1 className="text-gray-900 dark:text-gray-100 font-bold">iReapir</h1>
-      <nav className="flex flex-row text-gray-900 dark:text-gray-100 gap-3">
+      <nav className="inline-flex flex-wrap justify-end text-gray-900 dark:text-gray-100 gap-3">
         <Link to="/">Dashboard</Link>
         <Link to="/clients">Clientes</Link>
         <Link to="/service-orders">Ordens de Serviço</Link>
