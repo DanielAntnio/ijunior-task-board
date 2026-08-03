619223fc1d2092d1b4f7f86152c486f88b22739c
Daniel Antonio <danielantnio8679@gmail.com>
2026-08-03 14:31:05 -0300

    fix: corrige erro de digitação no Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 const Header = () => {
   return (
     <header className="inline-flex justify-between bg-slate-200 dark:bg-slate-900 px-2 py-0.5">
-      <h1 className="text-gray-900 dark:text-gray-100 font-bold">iReapir</h1>
+      <h1 className="text-gray-900 dark:text-gray-100 font-bold">iRepair</h1>
       <nav className="inline-flex flex-wrap justify-end text-gray-900 dark:text-gray-100 gap-3">
         <Link to="/">Dashboard</Link>
         <Link to="/clients">Clientes</Link>
