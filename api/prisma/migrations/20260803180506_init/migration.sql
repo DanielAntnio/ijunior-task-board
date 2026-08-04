9bd6b590fb47bbc6a4d39aca681b6e0d7653a8fd
Daniel Antonio <danielantnio8679@gmail.com>
2026-08-04 14:57:09 -0300

    chore(api): inicia e configura a base de dados prisma

---
+++ api/prisma/migrations/20260803180506_init/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE `Client` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `phone` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `ServiceOrder` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `client_id` INTEGER NOT NULL,
+    `issue` VARCHAR(191) NOT NULL,
+    `status` ENUM('OPEN', 'IN_PROGRESS', 'DONE') NOT NULL,
+    `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `ServiceOrder` ADD CONSTRAINT `ServiceOrder_client_id_fkey` FOREIGN KEY (`client_id`) REFERENCES `Client`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
