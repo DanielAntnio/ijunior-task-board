0a4323064586aad287265f2c3bd3181d580e2cdf
Daniel Antonio <danielantnio8679@gmail.com>
2026-08-09 11:36:31 -0300

    chore(api): adiciona tipagem para .env e req.user

---
+++ api/src/@types/global.d.ts
@@ -0,0 +1,25 @@
+import { Secret, SignOptions } from "jsonwebtoken";
+import { User } from "../../generated/prisma/client";
+
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+      DATABASE_URL: string;
+      DATABASE_USER: string;
+      DATABASE_PASSWORD: string;
+      DATABASE_NAME: string;
+      DATABASE_HOST: string;
+      JWT_SECRET: Secret;
+      JWT_EXPIRES_IN: SignOptions["expiresIn"];
+      NODE_ENV: "development" | "production" | "test";
+    }
+  }
+  namespace Express {
+    interface Request {
+      user?: Pick<User, "id" | "email">;
+    }
+  }
+}
+
+export {};
