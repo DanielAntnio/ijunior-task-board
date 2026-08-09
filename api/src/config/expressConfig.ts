caecaa15144cf533f4c49857d16e9a54f2b4fc34
Daniel Antonio <danielantnio8679@gmail.com>
2026-08-09 18:30:37 -0300

    feat(api): implementa cors

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import { ErrorHandler } from "../middlewares/error";
 import { PrismaErrorHandler } from "../middlewares/prisma-error";
 import { ZodErrorHandler } from "../middlewares/zod-erro";
@@ -10,6 +11,12 @@ import { authRoutes } from "../domains/auth/auth.router";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use("/auth", authRoutes);
 app.use("/clients", clientRoutes);
