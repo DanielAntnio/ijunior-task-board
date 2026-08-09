b82cec4648b7f69a1c08988f86df7ed0c36e8b99
Daniel Antonio <danielantnio8679@gmail.com>
2026-08-09 11:52:57 -0300

    feat(api): adiciona e implementa auth router

--- api/src/config/expressConfig.ts
+++ api/src/config/expressConfig.ts
@@ -1,13 +1,17 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import { ErrorHandler } from "../middlewares/error";
 import { PrismaErrorHandler } from "../middlewares/prisma-error";
 import { ZodErrorHandler } from "../middlewares/zod-erro";
 import { clientRoutes } from "../domains/clients/clients.router";
 import { serviceOrderRoutes } from "../domains/service-orders/service-order.router";
+import { authRoutes } from "../domains/auth/auth.router";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
+app.use("/auth", authRoutes);
 app.use("/clients", clientRoutes);
 app.use("/service_orders", serviceOrderRoutes);
 
