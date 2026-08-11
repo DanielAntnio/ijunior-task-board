183bdfb47f1cead093dd2b19f24b9ccf99fc972c
Daniel Antonio <danielantnio8679@gmail.com>
2026-08-11 10:20:46 -0300

    feat(api): aplica authMiddleware sobre clients

--- api/src/domains/clients/clients.router.ts
+++ api/src/domains/clients/clients.router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { ClientController } from "./clients.controller";
+import { authMiddleware } from "../../middlewares/authMiddleware";
 
 const clientRoutes = Router();
 const controller = new ClientController();
 
+clientRoutes.use(authMiddleware);
 clientRoutes.head("/:id", controller.idExist);
 
 clientRoutes.get("/", controller.list);
@@ -14,6 +16,5 @@ clientRoutes.post("/", controller.create);
 clientRoutes.put("/:id", controller.update);
 
 clientRoutes.delete("/:id", controller.delete);
-
 
 export { clientRoutes };
